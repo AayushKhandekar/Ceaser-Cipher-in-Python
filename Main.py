0db60c3b054ead3006a299c1e76fdf71d8c47e36
AayushKhandekar <akhandekar582@gmail.com>
2021-09-23 12:13:52 +0530

    updated code

--- Main.py
+++ Main.py
@@ -2,48 +2,64 @@ def encode(string, key):
     
     encodedString = '' 
     for i in string:
-        x = ord(i)      
+        x = ord(i) 
+        # Upper Case      
         if i.isupper():
             if key + x > 90 and x!= 32:
                 x = x + key - 26 
+            # Space 
             elif x != 32:
                 x = x + key
+        # Lower Case 
         else:
             if key + x > 122 and x != 32:
                 x = x + key - 26 
+            # Space 
             elif x != 32:
                 x = x + key
         encodedString += chr(x)
     print("Encoded String : ", encodedString)
 
 # Decoding error needs to be solved!
-# def decode(string, key):
+def decode(string, key):
     
-#     decodedString = ''
-#     for i in string:
-#         x = ord(i)
-#         if i.isupper():
-#             if abs(x - key) < 65 and x!= 32:
-#                 x = x - key + 26 
-#             elif x != 32:
-#                 x = x - key
-#         else:
-#             if key + x < 97 and x != 32:
-#                 x = x + key - 26 
-#             elif x != 32:
-#                 x = x + key
-#         decodedString += chr(x)
-#     print("Decoded String : ", decodedString)
+    decodedString = ''
+    for i in string:
+        x = ord(i)
+        # Upper Case 
+        if i.isupper():
+            if (x - key) < 65 and x != 32:
+                x = x - key + 26
+                print(x)
+            # Space
+            elif x != 32:
+                x = x - key
+        # Lower Case 
+        else:
+            if (x - key) < 97 and x != 32:
+                x = x - key + 26 
+            # Space
+            elif x != 32:
+                x = x - key
+        decodedString += chr(x)
+    print("Decoded String : ", decodedString)
 
+# Switch Case
 def switch(choice, string, key):
+
     if choice == 1:
         encode(string, key)
-    # elif choice == 2:
-        # decode(string, key)
+    elif choice == 2:
+        decode(string, key)
 
-if __name__ == "__main__":
-    
+if __name__ == "__main__": 
+
     string = input("Enter a String : ")
-    key = int(input("Enter a Key : "))    
+    key = int(input("Enter a Key : "))  
+
+    # Modifying 'key' into values less than 26
+    # Key values above 26 follow a circular pattern and again start from 1.
+    # To simplify, key = 1 is same as key = 27
+    key = key % 26  
     choice = int(input("1. Encode \n2. Decode \n"))
     switch(choice, string, key)
---
+++ README.md
@@ -0,0 +1,12 @@
+# Ceaser-Cipher-in-Python
+
+### What is Ceaser Cipher?
+
+A Caesar cipher is one of the simplest and most well-known encryption techniques. It is one of the oldest types of ciphers and is based on the simplest monoalphabetic cipher. It is considered a weak method of cryptography, as it is easy to decode the message owing to its minimum security techniques. Caesar cipher is often incorporated only in parts of other complex encryption schemes. 
+
+### To run :
+
+`python main.py`
+
+
+
